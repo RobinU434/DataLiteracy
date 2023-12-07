552a7b7ee515f4f60feaaa098cc656ea9d2a3838
Robin Uhrich <robin.uhrich@gmail.com>
2023-12-08 00:05:46 +0100

    renamed wrapper to manager

--- project/crawler/manager.py
+++ project/crawler/manager.py
@@ -13,15 +13,17 @@ class CrawlerManager:
     ) -> None:
         self._crawler = crawler
         self._pull_time = pull_time
+        self._sleep_interval = 1
 
     def start(self):
-        schedule.every().day(self._pull_time).do(self.process)
+        schedule.every().day.at(self._pull_time).do(self.process)
+        # schedule.every(3).seconds.do(self.process)
 
+        print("Job: runs .....")
         while True:
             schedule.run_pending()
             time.sleep(self._sleep_interval)
 
     def process(self):
         for crawler in self._crawler:
-            content = crawler.get()
-            crawler.write()
+            content = crawler.get(save=True)
