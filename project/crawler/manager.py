f58f4ecf4df3501fff0d98cc62a1bae757853e9c
Robin Uhrich <robin.uhrich@gmail.com>
2024-01-29 02:55:59 +0100

    str method for crawler manager

--- project/crawler/manager.py
+++ project/crawler/manager.py
@@ -16,10 +16,11 @@ class CrawlerManager:
         self._sleep_interval = 1
 
     def start(self):
-        schedule.every().day.at(self._pull_time).do(self.process)
+        job = schedule.every().day.at(self._pull_time).do(self.process)
         # schedule.every(3).seconds.do(self.process)
 
         print("Job: runs .....")
+        print(job)
         while True:
             schedule.run_pending()
             time.sleep(self._sleep_interval)
@@ -27,3 +28,12 @@ class CrawlerManager:
     def process(self):
         for crawler in self._crawler:
             content = crawler.get(save=True)
+
+    def __str__(self) -> str:
+        s = "included crawler: [\n "
+        for c in self._crawler:
+            s += str(c) + "\n"
+        s += "] \n"
+        s += "query time: " + self._pull_time
+        return s
+
